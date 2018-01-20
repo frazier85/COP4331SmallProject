9bdc3425e7e712966d6bba2ec9cd34f3ded60655
Goigle <goigle@codethree.net>
2018-01-20 17:11:39 -0500

    boilerplate code

--- api/global.php
+++ api/global.php
@@ -4,7 +4,25 @@ if(!defined("IN_API"))
 	die("Direct access of this file is now allowed");
 }
 
-$db_User = "root";
-$db_Name = "contactmanager";
-$db_Pass = "manageMe!";
+define("DB_HOST", "localhost");
+define("DB_NAME", "contactmanager");
+define("DB_USER", "root");
+define("DB_PASS", "manageMe!");
+
+
+function getRequestInfo()
+{
+	return json_decode(file_get_contents('php://input'), true);
+}
+
+function sendResultInfoAsJson( $obj )
+{
+	header('Content-type: application/json');
+	echo $obj;
+}
+
+function sendError($error)
+{
+	sendResultInfoAsJson('{"error":"' . $error . '"}');
+}
 ?>
