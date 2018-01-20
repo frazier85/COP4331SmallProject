93d5269a3a258b36d5ba3731049917540fead8cb
Goigle <goigle@codethree.net>
2018-01-20 16:51:57 -0500

    Beginning work on the login part of the API

--- api/login.php
+++ api/login.php
@@ -1,4 +1,61 @@
 <?PHP
 define("IN_API", 1);
 require_once "global.php";
+
+$login    = $_GET["login"];
+$logout   = $_GET["logout"];
+$register = $_GET["register"];
+
+$inData = getRequestInfo();
+$user = $inData["username"];
+$pass = $inData["password"];
+
+if(!isset($user) or !isset($pass))
+{
+	die("Username or password are mandatory");
+}
+
+$dbc = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+if (mysqli_connect_errno())
+{
+	echo "Error: " . mysqli_connect_errno;
+	die('');
+}
+
+if(isset($login)
+{
+	if ($stmt = $dbc->prepare("SELECT ID,username FROM USERS WHERE username=? AND PW=?" ))
+	{
+		$stmt->bind_param('ss', $user, $pass);
+		$stmt->execute();
+		$stmt->store_result();
+		$stmt->bind_result($uid,$user);
+		$json = '{"id":-1,"username":"","error":"Invalid username or password."}';
+		if($stmt->fetch())
+		{
+			$json = '{"id":' . $uid . ',"username":"' . $user .'",error":""}';
+		}
+		$stmt->close();
+		sendResultInfoAsJson($json);
+	}
+	else
+	{
+		printf("%s\n", $mysqli->error);
+	}
+}
+elseif(isset($logout))
+{
+	
+}
+elseif(isset($register))
+{
+	
+}
+else
+{
+	mysqli_close($dbc);
+	die("Invalid GET parameters.");
+}
+
+mysqli_close($dbc);
 ?>
