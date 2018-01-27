07938216fc07ba36751d1b0565282823cf2a221b
hispazn <ejortiz96@gmail.com>
2018-01-26 22:55:22 -0500

    added functions for contact.php

--- contactManager.js
+++ contactManager.js
@@ -1,16 +1,49 @@
 
-//  Ids and variable names based on what I saw from the html and php tags
-//  not entire sure how well everything is going to go together, feel free to change
-//  need to fix searh contacts so it works on every column (first, last, phone#, email)
-
 /*Hey EJ, hope you dont mind that I changed some variables and other things within the code to reflect what James
 said to me this afternoon about how he set up his php files and some variables I changed within the html document.
 Added the hideorshow back to make it so that everything can be done on one page
 -Katie*/
 
+// No problem, sounds great!
+// I changed the search function on here since it looks like the php will handle it ?
+// Additionally, added the register, delete, view, edit functions to pass the parameters based on the examples
+// updated ID names
+
 var urlBase = '/api';
 
 var userId = 0;
+
+function doRegister()
+{
+	var username = document.getElementById("loginUsername").value;
+	var password = document.getElementById("loginPassword").value;
+
+	var jsonPayload = '{"username" : "' + username + '", "password" : "' + password + '"}';
+	var url = urlBase + '/contact.php?register=1';
+
+	var xhr = new XMLHttpRequest();
+	xhr.open("POST", url, true);
+	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+	try
+	{
+		// Not sure if we want to display something upon completing register
+		// Wasn't sure where the ID tag was to edit the string so just commented it out for now
+		
+		/*xhr.onreadystatechange = function()
+		{
+			if (this.readyState == 4 && this.status == 200)
+			{
+				document.getElementById("contactSearchResult").innerHTML = "Registration complete";
+			}
+		};*/
+		xhr.send(jsonPayload);
+	}
+	catch(err)
+	{
+		document.getElementById("contactSearchResult").innerHTML = err.message;
+	}
+
+}
 
 function doLogin()
 {
@@ -101,29 +134,24 @@ function addContact()
 		{
 			if (this.readyState == 4 && this.status == 200)
 			{
-				document.getElementById("contactResult").innerHTML = "Contact has been added";
+				document.getElementById("contactSearchResult").innerHTML = "Contact has been added;
 			}
 		};
 		xhr.send(jsonPayload);
 	}
 	catch(err)
 	{
-		document.getElementById("contactResult").innerHTML = err.message;
+		document.getElementById("contactSearchResult").innerHTML = err.message;
 	}
 
 }
 
-// Searches by first name only for now, due to First name being the first column
-// need to have it compare information in other <td> cells of each <tr>
-function searchContact()
+function delContact()
 {
-	var search = document.getElementById("exampleInputEmail").value;
-	var contactList = document.getElementByTagName("table");
- 	var filter = search.value.toUpperCase();
-  	var tr = conactList.getElementsByTagName("tr");
+	var contactid = document.getElementById("inputFirstName").value;
 
-	var jsonPayload = '{"search" : "' + search + '"}';
-	var url = urlBase + '/search.' + extension;
+	var jsonPayload = '{"contactid" : "' + contactid + '"}';
+	var url = urlBase + '/contact.php?del=1';
 
 	var xhr = new XMLHttpRequest();
 	xhr.open("POST", url, true);
@@ -134,26 +162,103 @@ function searchContact()
 		{
 			if (this.readyState == 4 && this.status == 200)
 			{
-				var jsonObject = JSON.parse( xhr.responseText );
-
-				for (i = 0; i < tr.length; i++)
-				{
-				    td = tr[i].getElementsByTagName("td")[0];
-				    if (td)
-				    {
-				      	if (td.innerHTML.toUpperCase().indexOf(filter) > -1)
-				        	tr[i].style.display = "";
-				    	else {
-				        	tr[i].style.display = "none";
-				    }
-				}
+				document.getElementById("contactSearchResult").innerHTML = "Contact has been deleted";
 			}
 		};
 		xhr.send(jsonPayload);
 	}
 	catch(err)
 	{
-		document.getElementById("contactResult").innerHTML = err.message;
+		document.getElementById("contactSearchResult").innerHTML = err.message;
+	}
+
+}
+
+function editContact()
+{
+	var contactid = document.getElementById("searchText").value;
+	var fname = document.getElementById("inputFirstName").value;
+	var lname = document.getElementById("inputLastName").value;
+	var phone = document.getElementById("inputPhone").value;
+	var email = document.getElementById("inputEmail").value;
+
+	var jsonPayload = '{"contactid" : "' + contactid + '", "fname" : "' + fname + '", "lname" : "' + lname + '", "phone" : "' + phone + '", "email" : "' + email + '"}';
+	var url = urlBase + '/contact.php?edit=1';
+
+	var xhr = new XMLHttpRequest();
+	xhr.open("POST", url, true);
+	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+	try
+	{
+		xhr.onreadystatechange = function()
+		{
+			if (this.readyState == 4 && this.status == 200)
+			{
+				document.getElementById("contactSearchResult").innerHTML = "Contact has been updated";
+			}
+		};
+		xhr.send(jsonPayload);
+	}
+	catch(err)
+	{
+		document.getElementById("contactSearchResult").innerHTML = err.message;
+	}
+
+}
+
+
+function viewContact()
+{
+	var uid = document.getElementById("searchText").value;
+
+	var jsonPayload = '{"uid" : "' + uid + '"}';
+	var url = urlBase + '/contact.php?view=1';
+
+	var xhr = new XMLHttpRequest();
+	xhr.open("POST", url, true);
+	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+	try
+	{
+		xhr.onreadystatechange = function()
+		{
+			if (this.readyState == 4 && this.status == 200)
+			{
+				document.getElementById("contactSearchResult").innerHTML = "Contact has been retrieved";
+			}
+		};
+		xhr.send(jsonPayload);
+	}
+	catch(err)
+	{
+		document.getElementById("contactSearchResult").innerHTML = err.message;
+	}
+
+}
+
+function searchContact()
+{
+	var search = document.getElementById("searchText").value;
+
+	var jsonPayload = '{"search" : "' + search + '"}';
+	var url = urlBase + '/contact.php?search=1';
+
+	var xhr = new XMLHttpRequest();
+	xhr.open("POST", url, true);
+	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+	try
+	{
+		xhr.onreadystatechange = function()
+		{
+			if (this.readyState == 4 && this.status == 200)
+			{
+				document.getElementById("contactSearchResult").innerHTML = "Contact has been retrieved";
+			}
+		};
+		xhr.send(jsonPayload);
+	}
+	catch(err)
+	{
+		document.getElementById("contactSearchResult").innerHTML = err.message;
 	}
 
 }
